571a0f9eeff6b8d0c0d6e6d9d9d57879c8be2ccb
sarah <smile-wu@hotmail.com>
2017-10-08 16:48:28 +0800

    add product_list model and set relationship with order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :product_lists
 
 #  validates_presence_of :billing_name, :billing_address, :shipping_name, :shipping_address
 
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
-  validates :shipping_address, presence: true 
+  validates :shipping_address, presence: true
 
 end
