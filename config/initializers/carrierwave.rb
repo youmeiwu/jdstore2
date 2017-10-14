f2a8976a865a8424cecd9fade59e09255d79958f
sarah <smile-wu@hotmail.com>
2017-10-14 12:21:05 +0800

    modify carrierwave.rb file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,11 +4,11 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS-SECRET-ACCESS_KEY"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: ["AWS_REGION"]
     }
     config.fog_directory = ENV["AWS_BUCKET_NAME"]
   else
-    config.storage :file     
+    config.storage :file
   end
 end
